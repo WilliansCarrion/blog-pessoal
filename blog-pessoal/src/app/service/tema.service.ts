15492552b8ec28ba2d62a124a6cf98cd759d1349
WILLIANS CARRION <willianscarrion@gmail.com>
2020-09-03 15:12:37 -0300

    Implementação dos métodos PUT e DELETE

--- blog-pessoal/src/app/service/tema.service.ts
+++ blog-pessoal/src/app/service/tema.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Tema } from '../model/Tema';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -21,7 +22,15 @@ export class TemaService {
     return this.http.get(`http://localhost:8080/tema/${id}`, this.token)
   }
 
-    postTema(tema: Tema) {
+  postTema(tema: Tema) {
       return this.http.post('http://localhost:8080/tema', tema, this.token)
-    }
+  }
+
+  putTema(tema: Tema) {
+      return this.http.put('http://localhost:8080/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+        return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+  }
 }
