0aff1223ba9a0fce0ee87f0077477f6b378a8770
WILLIANS CARRION <willianscarrion@gmail.com>
2020-09-04 13:18:25 -0300

    Implementação de alerts reutilizáveis e pesquisa por tema e postagens

--- blog-pessoal/src/app/feed/feed.component.ts
+++ blog-pessoal/src/app/feed/feed.component.ts
@@ -3,6 +3,8 @@ import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
+import { AlertasService } from '../service/alertas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feed',
@@ -16,18 +18,30 @@ export class FeedComponent implements OnInit {
 
     postagem: Postagem = new Postagem()
     listaPostagens: Postagem[]
+    titulo: string
 
     tema: Tema = new Tema()
     listaTemas: Tema[]
-
     idTema: number
+    nomeTema: string
+
 
   constructor(
     private postagemService: PostagemService,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alert: AlertasService,
+    private router: Router
   ) { }
 
   ngOnInit() {
+
+    let token = localStorage.getItem('token')
+
+    if (token == null) {
+      this.router.navigate(['/login'])
+      this.alert.showAlertDanger('Faça o login antes de visitar o feed')
+      
+    }
     window.scroll(0, 0)
 
     this.findAllPostagens()
@@ -43,12 +57,12 @@ export class FeedComponent implements OnInit {
     this.postagem.tema = this.tema
 
     if (this.postagem.titulo == null || this.postagem.texto == null || this.postagem.tema == null) {
-      alert('Preenchimento obrigatório de todos os campos')
+      this.alert.showAlertInfo('Preenchimento obrigatório de todos os campos')
     } else {
       this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
       this.postagem = resp
       this.postagem = new Postagem()
-      alert ('Postagem realizada com sucesso!')
+      this.alert.showAlertSuccess('Postagem realizada com sucesso!')
       this.findAllPostagens()
       })
     }
@@ -61,5 +75,25 @@ export class FeedComponent implements OnInit {
   findByIdTema() {
     this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {this.tema = resp})
   }
+
+  findByTituloPostagem() {
+    if (this.titulo === ' ') {
+      this.findAllPostagens()
+    } else {
+      this.postagemService.getByTituloPostagem(this.titulo).subscribe((resp: Postagem[]) => {
+        this.listaPostagens = resp
+      })
+    }
+  }
+
+    findByNomeTema() {
+      if (this.nomeTema === ' ') {
+        this.findAllTemas()
+      } else {
+        this.temaService.getByNomeTema(this.nomeTema).subscribe((resp: Tema[]) => {
+          this.listaTemas = resp
+        })
+      }
+    }
 
 }
